1c78fb14ea8e59dd1599c03abffc3952358ad205
Hruday Chowdary Gurijala <gurijalahruday@gmail.com>
2024-12-17 10:12:10 +0530

    final commit v1.0

--- App.tsx
+++ App.tsx
@@ -1,27 +1,45 @@
-import { ScrollView, StyleSheet, Text, View,TextInput,TouchableOpacity,Alert } from 'react-native'
-import React, { useState, useEffect } from 'react'
+import { ScrollView, StyleSheet, Text, View,TextInput,TouchableOpacity,Alert } from 'react-native';
+import React, { useState } from 'react';
 
 const App = () => {
-  const [player1Name,setPlayer1Name] = useState('')
-  const [player2Name,setPlayer2Name] = useState('')
-  const [player1Points,setPlayer1Points] = useState(0)
-  const [player2Points,setPlayer2Points] = useState(0)
-  const [player1SetPoints,setPlayer1SetPoints] = useState(0)
-  const [player2SetPoints,setPlayer2SetPoints] = useState(0)
-  const [turnTextBox, setTurnTextBox] = useState(`${player1Name}' serve`)
-  useEffect(() => {
-    setTurnTextBox(`${player1Name}'s serve`);
-  }, [player1Name]);
-  
+  const [player1Name,setPlayer1Name] = useState('');
+  const [player2Name,setPlayer2Name] = useState('');
+  const [player1Points,setPlayer1Points] = useState(0);
+  const [player2Points,setPlayer2Points] = useState(0);
+  const [player1SetPoints,setPlayer1SetPoints] = useState(0);
+  const [player2SetPoints,setPlayer2SetPoints] = useState(0);
+  const [turnTextBox, setTurnTextBox] = useState('Player 1\'s serve');
+  const [mode,setMode] = useState('Play Duos');
+  const [turns,setTurns] = useState(2);
 
-
-  const playerTurn = (player1:string, player2:string, play1Points:number, play2Points:number)=>{
-    if((play1Points + play2Points) % 2 === 0){
-      setTurnTextBox(`${player1}'s serve`)
+  const gameMode = () =>{
+    resetGame();
+    if(mode === 'Play Duos'){
+      setTurns(4);
+      setMode('Play Singles');
     }else{
-    setTurnTextBox(`${player2}'s serve`)
+      setTurns(2);
+      setMode('Play Duos');
     }
-  }
+  };
+
+
+  const playerTurn = (player1:string, player2:string, play1Points:number, play2Points:number,nturns:number)=>{
+    if((play1Points + play2Points) % (nturns * 2) < nturns){
+      if(!player1){
+        setTurnTextBox('Player 1\'s serve');
+      }
+      else{
+        setTurnTextBox(`${player1}'s serve`);
+      }
+    }else{
+      if(!player2){
+        setTurnTextBox('Player 2\'s serve');
+      }else{
+        setTurnTextBox(`${player2}'s serve`);
+      }
+    }
+  };
 
   const showAlertAtEleven = (playerName:string) => {
     Alert.alert('Set completed.', `${playerName} has won the set!`, [
@@ -31,38 +49,58 @@ const App = () => {
 
   const onPlayer1 = () => {
     setPlayer1Points((prevCount) => {
-      playerTurn(player1Name,player2Name,player1Points+1,player2Points);
+    const newCount = prevCount + 1;
+    playerTurn(player1Name, player2Name, newCount, player2Points,turns);
       if (prevCount >= 10) {
-        showAlertAtEleven(player1Name); 
+        showAlertAtEleven(player1Name);
         setPlayer1SetPoints(count => count + 1);
-        return 0; 
+        resetSet();
+        return 0;
       }
-      return prevCount + 1; 
+      return prevCount + 1;
     });
   };
   const onPlayer2 = () => {
     setPlayer2Points((prevCount) => {
-      playerTurn(player1Name,player2Name,player1Points,player2Points+1);
+      const newCount = prevCount + 1;
+      playerTurn(player1Name, player2Name, player1Points, newCount,turns);
       if (prevCount >= 10) {
-        showAlertAtEleven(player2Name); 
+        showAlertAtEleven(player2Name);
         setPlayer2SetPoints(count => count + 1);
-        return 0; 
+        resetSet();
+        return 0;
       }
-      return prevCount + 1; 
+      return prevCount + 1;
     });
   };
   const player1Dec = ()=>setPlayer1Points((count) => (count > 0 ? count - 1 : 0));
   const player2Dec = ()=>setPlayer2Points((count) => (count > 0 ? count - 1 : 0));
 
+  const resetSet = ()=>{
+    setPlayer1Points(0);
+    setPlayer2Points(0);
+  };
+
+  const resetGame = () => {
+    setPlayer1Name('');
+    setPlayer2Name('');
+    setPlayer1Points(0);
+    setPlayer2Points(0);
+    setPlayer1SetPoints(0);
+    setPlayer2SetPoints(0);
+    setTurnTextBox('Player 1\'s serve');
+  };
+
+
   return (
     <ScrollView style={styles.container}>
       <View style={styles.navbar}>
-        <Text style={styles.heading}>Score Tracker </Text>
+        <Text style={styles.heading}>Ping Pong Score</Text>
       </View>
       <View style={styles.scoreBoard}>
             <View style={[styles.playerScore,styles.playerOne]}>
               <View>
-                <TextInput style={styles.name} placeholder='player 1'value={player1Name} onChangeText={(text)=>setPlayer1Name(text)}/>
+                <TextInput style={styles.name} maxLength={10} placeholder="Enter player"value={player1Name} onChangeText={(text)=>setPlayer1Name(text)} placeholderTextColor={'#ffffff'}/>
               </View>
               <View style={styles.pointContainer}>
                 <TouchableOpacity onPress={onPlayer1}>
@@ -76,7 +114,7 @@ const App = () => {
 
             <View style={[styles.playerScore,styles.playerTwo]}>
               <View>
-                <TextInput style={styles.name} placeholder='player 2'value={player2Name} onChangeText={(text)=>setPlayer2Name(text)}/>
+                <TextInput style={styles.name} maxLength={10} placeholder="Enter player"value={player2Name} onChangeText={(text)=>setPlayer2Name(text)} placeholderTextColor={'#ffffff'}/>
               </View>
               <View style={styles.pointContainer}>
                 <TouchableOpacity onPress={onPlayer2}>
@@ -104,32 +142,45 @@ const App = () => {
           </View>
       </View>
       <View style={styles.featureContainer}>
-      <View style={styles.reset}>
-        <Text style={styles.resetText}>Reset</Text>
-      </View>
-      <View style={styles.mode}><Text style={styles.modeText}>Duos</Text></View>
+      <TouchableOpacity onPress={resetGame}>
+        <View style={styles.reset}>
+          <Text style={styles.resetText}>Reset</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={gameMode}>
+      <View style={styles.mode}><Text style={styles.modeText}>{mode}</Text></View>
+      </TouchableOpacity>
       </View>
     </ScrollView>
-  )
-}
+  );
+};
+
+//#d4e7f5
+//#fe5f56
+//#05c5f5
+//#fefffe
 
 export default App;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#ffffff',
+    backgroundColor:'#fefffe',
   },
-  navbar:{
-    backgroundColor: '#ff0000',
-    color: '#ffffff',
+  navbar: {
+    backgroundColor: '#d4e7f5',
+    color: '#42515a',
+    paddingVertical: 20,
   },
-  heading:{
-    color: '#ffffff',
-    fontSize: 50,
-    fontWeight:500,
+  heading: {
+    paddingHorizontal:15,
+    color: '#42515a',
+    fontSize: 40,
+    fontWeight: '500',
   },
   scoreBoard:{
     margin:30,
+    marginTop:50,
     display:'flex',
     flexDirection:'row',
     alignItems:'center',
@@ -139,23 +190,28 @@ const styles = StyleSheet.create({
   playerScore:{
     height:300,
     width:175,
-    backgroundColor:'#f0f0f0',
+    // backgroundColor:'#f0f0f0',
     borderRadius:10,
     flex:1,
+    backgroundColor:'#cad5e2',
+    elevation:4,
   },
   playerTwo:{
-
+    backgroundColor: '#fe5f56',
   },
   playerOne:{
-
+    backgroundColor:'#05c5f5',
   },
   name:{
-    padding:15,
-    borderBottomWidth: 5,
-
+    margin:7,
+    padding:10,
+    borderBottomWidth: 3,
+    borderColor:'#ffffff',
+    fontSize:25,
+    color:'#ffffff',
   },
   pointContainer:{
-    backgroundColor: '#f0f0f0',
+    backgroundColor:'transparent',
     padding: 0,
     justifyContent:'center',
     borderRadius:10,
@@ -168,7 +224,8 @@ const styles = StyleSheet.create({
   points:{
     fontWeight: 'bold',
     fontSize: 100,
-    paddingBottom:50,
+    paddingBottom:60,
+    color:'#fffdff',
   },
   setPoints:{
     height:80,
@@ -176,10 +233,10 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 0,
     right: 0,
-    backgroundColor: '#007bff',
+    backgroundColor: '#335773',
     paddingHorizontal: 10,
     paddingVertical: 5,
-    borderRadius: 10,
+    borderRadius:10,
     justifyContent:'center',
     alignItems:'center',
   },
@@ -189,7 +246,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 0,
     left: 0,
-    backgroundColor: '#007bff',
+    backgroundColor: '#335773',
     paddingHorizontal: 10,
     paddingVertical: 5,
     borderRadius: 10,
@@ -199,6 +256,7 @@ const styles = StyleSheet.create({
   setPointsText:{
     fontWeight:'bold',
     fontSize:25,
+    color:'#fffdff',
   },
   ControlComm:{
     marginHorizontal:30,
@@ -214,17 +272,21 @@ const styles = StyleSheet.create({
     width:70,
     height:70,
     borderRadius: 35,
-    backgroundColor:'red',
+    backgroundColor:'#fe5f56',
+    elevation:5,
   },
   turn:{
     width:200,
-    borderWidth:1,
     height:50,
-    borderRadius:10,
+    alignItems:'center',
+    justifyContent:'center',
   },
   turnText:{
-    fontSize:18,
+    fontSize:25,
     padding:5,
+    color:'#335773',
+    fontStyle:'italic',
+    fontWeight:'bold',
   },
   decrement:{
     fontSize:50,
@@ -247,14 +309,15 @@ const styles = StyleSheet.create({
     width:150,
     backgroundColor:'#f0f0f0',
     borderRadius:10,
+    elevation:5,
   },
   resetText:{
     fontSize:20,
-    fontWeight:'bold'
+    fontWeight:'bold',
   },
   modeText:{
     fontSize:20,
-    fontWeight:'bold'
+    fontWeight:'bold',
   },
   mode:{
     display:'flex',
@@ -264,5 +327,6 @@ const styles = StyleSheet.create({
     width:150,
     backgroundColor:'#f0f0f0',
     borderRadius:10,
-  }
+    elevation:5,
+  },
 })
